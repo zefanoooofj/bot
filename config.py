f389306478f03558d80ba4a5c17d5a8d58b64b84
ASUS@LAPTOP-K0E9796A <richarddamarjanan@gmail.com>
2022-11-11 20:23:46 +0700

    first

---
+++ config.py
@@ -0,0 +1,6 @@
+consumer_key = "qh77HRwBsrnWe0x7t0EweTHoI"
+consumer_secret = "i9y849kaHUH25ljg9UrKVpjanT9W601hdPD3sdKkISTUdOcNm9"
+access_token = "1590936840684548096-GGzRrYS4SXdwG3GAEPBFCsJwohl9Y9"
+access_token_secret = "2Bd3PhCtsPHmVip1CqpxN5S4ndtwgT9uvCEtZb6v49P4L"
+trigger = "kf!"
+timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
